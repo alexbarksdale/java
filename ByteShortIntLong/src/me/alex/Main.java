446894036076bd54c5a30ed76e62aeadf8e35dcb
alexbarksdale <ale.barksdale@gmail.com>
2020-07-27 20:31:55 -0500

    Basics

---
+++ ByteShortIntLong/src/me/alex/Main.java
@@ -0,0 +1,30 @@
+package me.alex;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int minVal = Integer.MIN_VALUE;
+        int maxVal = Integer.MAX_VALUE;
+
+        System.out.println("Min Val:" +  minVal);
+        System.out.println("Max Val:" +  maxVal);
+
+        byte minByteVal = Byte.MIN_VALUE;
+        byte maxByteVal = Byte.MAX_VALUE;
+
+        System.out.println("Min Val:" +  minByteVal);
+        System.out.println("Max Val:" +  maxByteVal);
+
+        short minShortVal = Short.MIN_VALUE;
+        short maxShortVal = Short.MAX_VALUE;
+
+        System.out.println("Min Val:" +  minShortVal);
+        System.out.println("Max Val:" +  maxShortVal);
+
+        long minLongVal = Long.MIN_VALUE;
+        long maxLongVal = Long.MAX_VALUE;
+
+        System.out.println("Min Val:" +  minLongVal);
+        System.out.println("Max Val:" +  maxLongVal);
+    }
+}
